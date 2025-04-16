ce0d1939d294ef6dd69bef82e4382a376ffad806
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-04-17 00:13:38 +0530

    completed city repositpory

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -23,6 +23,34 @@ class CityRepository{
             throw {error}
         }
     }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId)
+            return city
+        } catch (error) {
+            console.log("something went wrong in the repository layer");
+            throw {error};
+               
+        }
+
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where:{
+                    city:cityId
+                }
+            })
+            return city;
+            
+        } catch (error) {
+            console.log("something went wrong in the repository layer");
+            throw {error};
+        }
+
+    }
 }
 
 module.exports = CityRepository
