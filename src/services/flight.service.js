7c4a771a8e3553ae2b25d748a92ee0a21afba9c5
ansarialiakbar <ansarialiakbar2001@gmail.com>
2025-05-01 02:02:32 +0530

    complete flight create crud

--- src/services/flight.service.js
+++ src/services/flight.service.js
@@ -1,4 +1,5 @@
 const {FlightsRepository, AirplaneRepository} = require('../repository/index')
+const {compareTime} = require('../utils/helper')
 
 class FlightService{
     constructor() {
@@ -7,6 +8,9 @@ class FlightService{
     }
     async createFlight(data){
         try {
+            if(!compareTime(data.arrivalTime, data.departureTime)){
+                throw {error: 'arrivalTime cannot be less than departueTime'}
+            }
             const airplane = await this.airplaneRepository.getAirplane(data.airplaneId)
             const flight = await this.flightRepository.createFlight({
                 ...data, totalSeats:airplane.capacity
